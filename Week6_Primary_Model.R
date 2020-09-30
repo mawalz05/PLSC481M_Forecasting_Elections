18e62ff660cce1cf30d347dacda84d6760ace2c0
Matthew Walz <mawalz05@gmail.com>
2020-09-30 19:49:11 -0400

    Add files via upload

--- Week6_Primary_Model.R
+++ Week6_Primary_Model.R
@@ -3,7 +3,7 @@ library(dplyr)
 
 rm(list=ls())
 
-norpoth = read.csv("C:\\Users\\mawal\\Desktop\\Norpoth_data.csv")
+norpoth = read.csv("C:\\Users\\mawal\\OneDrive - Binghamton University\\Desktop\\Desktop_Folders\\Teaching\\Forecasting Elections\\Week 6\\Norpoth_data.csv")
 
 #Random Walk
 random_walk = lm(r_vote ~ r_vote_1, data = norpoth)
@@ -21,43 +21,49 @@ auto_reg$coeff[1] + norpoth[27, 7]*auto_reg$coeff[2] + norpoth[26, 7]*auto_reg$c
 
 
 
-#Removing the last two rows and first two columns (avg value and 2020) (year and primary vote)
-norpoth = norpoth[1:27, 3:13]
 
-#Dems Regression
-regd = lm(d_vote ~ inc_primary + noninc_primary + d_vote_1 + d_vote_2 + partisan_adj, data = norpoth)
-summary(regd)
+#Primary Model Regression
+regr = lm(d_vote ~ inc_primary + noninc_primary + r_vote_1 + r_vote_2 + partisan_adj, data = norpoth)
+summary(regr)
 
-Biden = -1*regd$coefficients[2]*(.856-mean(norpoth[, 1])) + regd$coefficients[3]*(.284-mean(norpoth[, 2])) + 
-  regd$coefficients[4]*norpoth[27, 9] + regd$coefficients[5]*norpoth[27, 10] + regd$coefficients[1]
-Biden
+Trump = regr$coefficients[2]*(.856-mean(norpoth[, 3])) + -1*regr$coefficients[3]*(.284-mean(norpoth[, 4])) + 
+  regr$coefficients[4]*norpoth[27, 8] + regr$coefficients[5]*norpoth[27, 9] + regr$coefficients[1]
+Trump
 
 #Changing the range of the primary support variables from 35-65
 for (i in 1:nrow(norpoth)){
-  if (norpoth[i,1] <.35) {
-    norpoth[[i, 1]] = .35
+  if (norpoth[i,3] <.35) {
+    norpoth[[i, 3]] = .35
   }
-  if (norpoth[i, 1] > .65) {
-    norpoth[[i, 1]] = .65
+  if (norpoth[i, 3] > .65) {
+    norpoth[[i, 3]] = .65
   }
 }
 
 for (i in 1:nrow(norpoth)){
-  if (norpoth[i,2] <.35) {
-    norpoth[[i, 2]] = .35
+  if (norpoth[i,4] <.35) {
+    norpoth[[i, 4]] = .35
   }
-  if (norpoth[i, 2] > .65) {
-    norpoth[[i, 2]] = .65
+  if (norpoth[i, 4] > .65) {
+    norpoth[[i, 4]] = .65
   }
 }
 
-regd = lm(d_vote ~ inc_primary + noninc_primary + d_vote_1 + d_vote_2 + partisan_adj, data = norpoth)
-summary(regd)
+#Rerunning the regressions
+regr = lm(d_vote ~ inc_primary + noninc_primary + r_vote_1 + r_vote_2 + partisan_adj, data = norpoth)
+summary(regr)
 
-Biden2 = -1*regd$coefficients[2]*(.65-mean(norpoth[, 1])) + regd$coefficients[3]*(.35-mean(norpoth[, 2])) + 
-  regd$coefficients[4]*norpoth[27, 9] + regd$coefficients[5]*norpoth[27, 10] + regd$coefficients[1]
-Biden2
+Trump = regr$coefficients[2]*(.65-mean(norpoth[, 3])) + -1*regr$coefficients[3]*(.35-mean(norpoth[, 4])) + 
+  regr$coefficients[4]*norpoth[27, 11] + regr$coefficients[5]*norpoth[27, 12] + regr$coefficients[1]
+Trump
 
-#These are the 2016 coefficients
--1*.417*(65-57) + .17*(35-48) + 51.1133*.361 + 52*-.377 + 50.6
+#Testing the mean values
+mean(norpoth[, 3])
+mean(norpoth[, 4])
+
+#Using the mean values from the article. 
+Trump = regr$coefficients[2]*(.65-.54) + -1*regr$coefficients[3]*(.35-.49) + 
+  regr$coefficients[4]*norpoth[27, 8] + regr$coefficients[5]*norpoth[27, 9] + regr$coefficients[1]
+Trump
+
 
